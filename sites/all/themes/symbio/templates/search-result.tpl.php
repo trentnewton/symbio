fa532aa372b36ea86ab29c5450b7ce199c1f0708
Trent Newton <trent99878@gmail.com>
2015-11-30 13:45:01 +1000

    modified indentation

--- sites/all/themes/symbio/templates/search-result.tpl.php
+++ sites/all/themes/symbio/templates/search-result.tpl.php
@@ -50,12 +50,12 @@
     <a href="<?php print $url; ?>"><?php print $title; ?></a>
   </h3>
   <div class="search-snippet-info">
-    <?php if ($info) : ?>
-      <p class="search-info"><?php print $info; ?></p>
-    <?php endif; ?>
-    <?php if ($snippet) : ?>
-      <p class="search-snippet"><?php print $snippet; ?></p>
-    <?php endif; ?>
+  <?php if ($info) : ?>
+    <p class="search-info"><?php print $info; ?></p>
+  <?php endif; ?>
+  <?php if ($snippet) : ?>
+    <p class="search-snippet"><?php print $snippet; ?></p>
+  <?php endif; ?>
   </div>
   <hr>
 </div>
