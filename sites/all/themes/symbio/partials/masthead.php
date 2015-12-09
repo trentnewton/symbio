7476e1167d583e9a7e692c75a4f50b183183a7d8
Trent Newton <trent99878@gmail.com>
2015-12-09 11:57:07 +1000

    tidy up of markup

--- sites/all/themes/symbio/partials/masthead.php
+++ sites/all/themes/symbio/partials/masthead.php
@@ -6,19 +6,16 @@
         <?php print render($page['masthead']); ?>
       </div>
       <?php endif; ?>
-    <?php
-    if (arg(0) == 'user' && is_numeric(arg(1)) && (arg(2) == NULL) ){ ?>
-      <?php if ($tabs): ?>
       <div class="masthead-nav" data-sr="enter bottom wait 0.5s">
+    <?php if (arg(0) == 'user' && is_numeric(arg(1)) && (arg(2) == NULL) ){ ?>
+      <?php if ($tabs): ?>
         <?php print render($tabs); ?>
-      </div>
       <?php endif; ?>
     <?php } else { ?>
       <?php if ($page['masthead_nav']): ?>
-      <div class="masthead-nav" data-sr="enter bottom wait 0.5s">
         <?php print render($page['masthead_nav']); ?>
-      </div>
       <?php endif; ?>
+      </div>
     <?php } ?>
     </div>
   </div>
