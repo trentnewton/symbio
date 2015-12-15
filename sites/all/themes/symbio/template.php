0f1bf70daa33f2c0da4e72afcaddb01dde320f01
Trent Newton <trent99878@gmail.com>
2015-12-15 23:36:22 +1000

    updated module support

--- sites/all/themes/symbio/template.php
+++ sites/all/themes/symbio/template.php
@@ -28,6 +28,7 @@ $exclude = array(
 'misc/jquery.js' => TRUE,
 'misc/jquery.once.js' => TRUE,
 'sites/all/modules/jquery_update/replace/jquery/1.10/jquery.min.js' => TRUE,
+'sites/all/modules/autoupload/js/autoupload.js' => TRUE,
 'misc/drupal.js' => TRUE, );
 
 $js = array_diff_key($js, $exclude);
