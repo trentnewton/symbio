652ada1c5984f8ef1048153836d4a31af15756ef
Trent Newton <trent99878@gmail.com>
2016-01-16 11:42:48 +1000

    small tidy up

--- sites/all/themes/symbio/templates/page--user--login.tpl.php
+++ sites/all/themes/symbio/templates/page--user--login.tpl.php
@@ -2,7 +2,7 @@
 <main id="login-body" itemscope itemprop="mainContentOfPage" itemtype="http://schema.org/WebPageElement">
   <header class="row column text-center">
     <h1 itemprop="headline"><?php print $title; ?></h1>
-  </header> 
+  </header>
   <article class="row">
     <div class="medium-6 small-centered columns login-form-box-wrapper">
       <div class="login-form-box text-center">
