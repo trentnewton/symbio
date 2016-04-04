f701fd0e2045d53d3480babc3e1f53f0288eecae
Trent Newton <trent99878@gmail.com>
2016-04-04 12:05:40 +1000

    removed space

--- sites/all/themes/symbio/template.php
+++ sites/all/themes/symbio/template.php
@@ -22,7 +22,7 @@ function symbio_process_html(&$vars) {
 
 // Remove Drupal core css
 
-function symbio_css_alter(&$css) { 
+function symbio_css_alter(&$css) {
   unset($css[drupal_get_path('module','system').'/system.theme.css']);
   unset($css[drupal_get_path('module','system').'/system.messages.css']);
   unset($css[drupal_get_path('module','system').'/system.menus.css']);
