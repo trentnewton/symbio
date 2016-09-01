fd20d3e4f4ce6c0ee9806613c1562c7035d3831d
Trent Newton <trent99878@gmail.com>
2016-09-01 16:54:31 +1000

    added fix for recaptcha not re-loading with ajax

--- sites/all/themes/symbio/src/assets/js/app.js
+++ sites/all/themes/symbio/src/assets/js/app.js
@@ -95,6 +95,20 @@
     }
   });
 
+  Drupal.behaviors.recapcha_ajax_behaviour = {
+    attach: function(context, settings) {
+      if (typeof grecaptcha != "undefined") {
+        var captchas = document.getElementsByClassName('g-recaptcha');
+        for (var i = 0; i < captchas.length; i++) {
+          var site_key = captchas[i].getAttribute('data-sitekey');
+          if (!$(captchas[i]).html()) {
+            grecaptcha.render(captchas[i], { 'sitekey' : site_key});
+          }
+        }
+      }
+    }
+  }
+
 })(jQuery);
 
 // animation settings
